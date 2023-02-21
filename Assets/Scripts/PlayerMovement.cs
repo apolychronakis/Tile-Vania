68f1a692384e3b0b734b15afecdc84766dce15e0
Andreas Polychronakis <andrewpolychronakis@gmail.com>
2023-02-22 00:52:25 +0200

    Input System. Player movement only horizontal.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float runSpeed = 10f;
+
+    Vector2 moveInput;
+    Rigidbody2D rb2D;
+
+
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2D.velocity.y);
+        rb2D.velocity = playerVelocity;
+    }
+}
