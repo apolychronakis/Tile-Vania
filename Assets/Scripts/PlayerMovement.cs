0de6240d9053953440b66d1b5f8fd717febe3ffc
Andreas Polychronakis <andrewpolychronakis@gmail.com>
2023-02-22 09:13:30 +0200

    Add jump mechanics. Add flip sprite and call correspond animation while moving.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,31 +9,58 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
 
     Vector2 moveInput;
-    Rigidbody2D rb2D;
-
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    Collider2D myCapsuleCollider;
 
 
     private void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
         Run();
+        FlipSprite();
     }
-
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2D.velocity.y);
-        rb2D.velocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHorizontalSpeed);
     }
+
+    void FlipSprite()
+    {
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if(playerHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
 }
